d55e7b63bd29b45ef834544ca5b54d53ccd1d16a
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-02-19 21:25:06 +0300

    base interface and const

--- backoff.go
+++ backoff.go
@@ -1,1 +1,13 @@
 package backoff
+
+import "time"
+
+const (
+	DefaultMinBackoff = 1 * time.Second
+	DefaultMaxBackoff = 20 * time.Second
+	DefaultStep       = 2.0
+)
+
+type Backoff interface {
+	Next(int64) time.Duration
+}
