ecefeb73dd45bc52c3d4b4ba5db978b071bd2137
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-02-19 23:03:44 +0300

    base full jitter test cases

--- full_jitter_test.go
+++ full_jitter_test.go
@@ -1,7 +1,47 @@
 package backoff
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestFullJitterStrategy(t *testing.T) {
 	t.Parallel()
+
+	t.Run("SuccessInitStrategy", func(t *testing.T) {
+		baseDelay := 1 * time.Second
+		maxDelay := 10 * time.Second
+
+		exponential := NewFullJitter(baseDelay, maxDelay)
+		assert.NotNil(t, exponential)
+
+		assert.Equal(t, baseDelay, exponential.baseDelay)
+		assert.Equal(t, maxDelay, exponential.maxDelay)
+	})
+
+	t.Run("InitStrategyWithDefaultValDelay", func(t *testing.T) {
+		exponential := NewFullJitter(0, 0)
+		assert.NotNil(t, exponential)
+
+		assert.Equal(t, DefaultMinBackoff, exponential.baseDelay)
+		assert.Equal(t, DefaultMaxBackoff, exponential.maxDelay)
+	})
+
+	t.Run("InitStrategyWithInvalidArgs", func(t *testing.T) {
+		exponential := NewFullJitter(0, -10*time.Second)
+		assert.NotNil(t, exponential)
+
+		assert.Equal(t, DefaultMinBackoff, exponential.baseDelay)
+		assert.Equal(t, DefaultMaxBackoff, exponential.maxDelay)
+	})
+
+	t.Run("InitStrategyWhenMaxLessThanBase", func(t *testing.T) {
+		baseDelay := 5 * time.Second
+		maxDelay := 1 * time.Second
+		jitter := NewFullJitter(baseDelay, maxDelay)
+
+		assert.Equal(t, baseDelay, jitter.maxDelay)
+	})
 }
