7a0285ceb5450e46e004dc3c137b5f2447bb5e52
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-02-19 23:10:14 +0300

    CheckMinDelay test case

--- full_jitter_test.go
+++ full_jitter_test.go
@@ -14,41 +14,41 @@ func TestFullJitterStrategy(t *testing.T) {
 		baseDelay := 1 * time.Second
 		maxDelay := 10 * time.Second
 
-		exponential := NewFullJitter(baseDelay, maxDelay)
-		assert.NotNil(t, exponential)
+		fullJitter := NewFullJitter(baseDelay, maxDelay)
+		assert.NotNil(t, fullJitter)
 
-		assert.Equal(t, baseDelay, exponential.baseDelay)
-		assert.Equal(t, maxDelay, exponential.maxDelay)
+		assert.Equal(t, baseDelay, fullJitter.baseDelay)
+		assert.Equal(t, maxDelay, fullJitter.maxDelay)
 	})
 
 	t.Run("InitStrategyWithDefaultValDelay", func(t *testing.T) {
-		exponential := NewFullJitter(0, 0)
-		assert.NotNil(t, exponential)
+		fullJitter := NewFullJitter(0, 0)
+		assert.NotNil(t, fullJitter)
 
-		assert.Equal(t, DefaultMinBackoff, exponential.baseDelay)
-		assert.Equal(t, DefaultMaxBackoff, exponential.maxDelay)
+		assert.Equal(t, DefaultMinBackoff, fullJitter.baseDelay)
+		assert.Equal(t, DefaultMaxBackoff, fullJitter.maxDelay)
 	})
 
 	t.Run("InitStrategyWithInvalidArgs", func(t *testing.T) {
-		exponential := NewFullJitter(0, -10*time.Second)
-		assert.NotNil(t, exponential)
+		fullJitter := NewFullJitter(0, -10*time.Second)
+		assert.NotNil(t, fullJitter)
 
-		assert.Equal(t, DefaultMinBackoff, exponential.baseDelay)
-		assert.Equal(t, DefaultMaxBackoff, exponential.maxDelay)
+		assert.Equal(t, DefaultMinBackoff, fullJitter.baseDelay)
+		assert.Equal(t, DefaultMaxBackoff, fullJitter.maxDelay)
 	})
 
 	t.Run("InitStrategyWhenMaxLessThanBase", func(t *testing.T) {
 		baseDelay := 5 * time.Second
 		maxDelay := 1 * time.Second
-		jitter := NewFullJitter(baseDelay, maxDelay)
+		fullJitter := NewFullJitter(baseDelay, maxDelay)
 
-		assert.Equal(t, baseDelay, jitter.maxDelay)
+		assert.Equal(t, baseDelay, fullJitter.maxDelay)
 	})
 
 	t.Run("ReturnsNextInStrictRange", func(t *testing.T) {
 		baseDelay := 100 * time.Millisecond
 		maxDelay := 10 * time.Second
-		jitter := NewFullJitter(baseDelay, maxDelay)
+		fullJitter := NewFullJitter(baseDelay, maxDelay)
 
 		// For attempt = 3:
 		// limit = 100ms * 2^3 = 800ms
@@ -56,7 +56,7 @@ func TestFullJitterStrategy(t *testing.T) {
 		maxExpected := 800 * time.Millisecond
 
 		for i := 0; i < 15; i++ {
-			delay := jitter.Next(3)
+			delay := fullJitter.Next(3)
 			assert.True(t, delay >= 0)
 			assert.True(t, delay <= maxExpected)
 		}
@@ -65,10 +65,17 @@ func TestFullJitterStrategy(t *testing.T) {
 	t.Run("CheckOverflow", func(t *testing.T) {
 		baseDelay := 1 * time.Second
 		maxDelay := 100 * time.Second
-		jitter := NewFullJitter(baseDelay, maxDelay)
+		fullJitter := NewFullJitter(baseDelay, maxDelay)
 
-		delay := jitter.Next(63)
+		delay := fullJitter.Next(63)
 		assert.LessOrEqual(t, delay, maxDelay)
 		assert.GreaterOrEqual(t, delay, time.Duration(0))
+	})
+
+	t.Run("CheckMinDelay", func(t *testing.T) {
+		fullJitter := &FullJitter{baseDelay: 0, maxDelay: 0}
+
+		delay := fullJitter.Next(5)
+		assert.Equal(t, time.Duration(0), delay)
 	})
 }
